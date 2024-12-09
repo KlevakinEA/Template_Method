c3f542278ee5d671cfd38a549597ee0fc6eea0ca
Nester <kyrydyndyndynkrdynkrdyna@gmail.com>
2024-12-09 13:51:44 +0700

    Добавьте файлы проекта.

---
+++ Template_Method/Program.cs
@@ -0,0 +1,31 @@
+﻿StudentReportGenerator s = new StudentReportGenerator();
+GroupReportGenerator g = new GroupReportGenerator();
+s.Export(s.Form(s.Compute(s.Collect())));
+g.Export(g.Form(g.Compute(g.Collect())));
+
+
+
+
+abstract class ReportGenerator
+{
+    abstract public string Collect();
+    abstract public string Compute(string input);
+    abstract public string Form(string input);
+    public void Export(string input) { Console.WriteLine(input); }
+}
+
+
+class StudentReportGenerator: ReportGenerator
+{
+    override public string Collect() { Console.WriteLine("Data collected"); return "Data"; }
+    override public string Compute(string input) { Console.WriteLine("Data computed"); return input + "_"; }
+    override public string Form(string input) { Console.WriteLine("Data Formed"); return input + "."; }
+}
+
+
+class GroupReportGenerator : ReportGenerator
+{
+    override public string Collect() { Console.WriteLine("Data collected"); return "Other data"; }
+    override public string Compute(string input) { Console.WriteLine("Data computed"); return input + " "; }
+    override public string Form(string input) { Console.WriteLine("Data Formed"); return input + ","; }
+}
